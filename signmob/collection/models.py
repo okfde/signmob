f98ef729afe32a4f23531c5a97ea8f5eba503af1
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-07-09 15:20:45 +0200

    Add meta info to collection models

--- signmob/collection/models.py
+++ signmob/collection/models.py
@@ -11,6 +11,13 @@ class CollectionGroupMember(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     joined = models.DateTimeField(default=timezone.now)
 
+    class Meta:
+        verbose_name = 'Ortsgruppenmitglied'
+        verbose_name_plural = 'Ortsgruppenmitglieder'
+
+    def __str__(self):
+        return self.user.name
+
 
 class CollectionGroup(models.Model):
     name = models.CharField(max_length=255, blank=True)
@@ -24,16 +31,12 @@ class CollectionGroup(models.Model):
         Calendar, null=True, blank=True, on_delete=models.SET_NULL
     )
 
+    class Meta:
+        verbose_name = 'Ortsgruppe'
+        verbose_name_plural = 'Ortsgruppen'
+
     def __str__(self):
         return self.name
-
-
-class CollectionEventMember(models.Model):
-    group = models.ForeignKey("CollectionEvent", on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    start = models.DateTimeField(null=True, blank=True)
-    end = models.DateTimeField(null=True, blank=True)
 
 
 class CollectionLocation(models.Model):
@@ -53,8 +56,27 @@ class CollectionLocation(models.Model):
     )
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
 
+    class Meta:
+        verbose_name = 'Sammelort'
+        verbose_name_plural = 'Sammelorte'
+
     def __str__(self):
         return self.name
+
+
+class CollectionEventMember(models.Model):
+    event = models.ForeignKey("CollectionEvent", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    start = models.DateTimeField(null=True, blank=True)
+    end = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Sammelterminteilnehmer/in'
+        verbose_name_plural = 'Sammelterminteilnehmende'
+
+    def __str__(self):
+        return '{} bei {}'.format(self.user, self.event)
 
 
 class CollectionEvent(models.Model):
@@ -63,10 +85,19 @@ class CollectionEvent(models.Model):
 
     geo = models.PointField(null=True, blank=True, geography=True)
 
+    group = models.ForeignKey(
+        CollectionGroup, null=True, blank=True,
+        on_delete=models.SET_NULL
+    )
+
     event_occurence = models.ForeignKey(
         Occurrence, null=True, blank=True, on_delete=models.SET_NULL
     )
     members = models.ManyToManyField(User, through=CollectionEventMember)
+
+    class Meta:
+        verbose_name = 'Sammeltermin'
+        verbose_name_plural = 'Sammeltermine'
 
     def __str__(self):
         return self.name
@@ -91,6 +122,10 @@ class CollectionResult(models.Model):
     event = models.ForeignKey(
         CollectionEvent, null=True, blank=True, on_delete=models.SET_NULL
     )
+
+    class Meta:
+        verbose_name = 'Sammelergebnis'
+        verbose_name_plural = 'Sammelergebnisse'
 
     def __str__(self):
         return '{} ({} - {})'.format(self.amount, self.start, self.end)
