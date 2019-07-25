a326a5c8c7a0859f2248530795c2f6477f762eb3
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-07-25 20:03:14 +0200

    Fix LOGIN_REDIRECT_URL

--- config/settings/base.py
+++ config/settings/base.py
@@ -102,7 +102,7 @@ AUTHENTICATION_BACKENDS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#auth-user-model
 AUTH_USER_MODEL = "users.User"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-redirect-url
-LOGIN_REDIRECT_URL = "user:redirect"
+LOGIN_REDIRECT_URL = "collection:home"
 # https://docs.djangoproject.com/en/dev/ref/settings/#login-url
 LOGIN_URL = "account_login"
 
