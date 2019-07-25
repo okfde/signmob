002d8cf66eeabd5fe34c0ba72a4aafd952da9691
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-07-25 18:21:47 +0200

    Remove superfluous setting

--- config/settings/production.py
+++ config/settings/production.py
@@ -103,7 +103,6 @@ EMAIL_PORT = env('DJANGO_EMAIL_PORT')
 EMAIL_HOST_USER = env('DJANGO_EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('DJANGO_EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = True
 
 # ADMIN
 # ------------------------------------------------------------------------------
