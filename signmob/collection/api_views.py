80d44b602e98977a9b05d81c18d02f2efd8f639e
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-07-25 15:59:53 +0200

    Improve API with details

--- signmob/collection/api_views.py
+++ signmob/collection/api_views.py
@@ -1,7 +1,12 @@
-from django.db.models import Value, CharField
+from datetime import timedelta
+
+from django.db.models import (
+    F, Value, CharField, IntegerField, DateTimeField
+)
 from django.conf import settings
 from django.urls import reverse
-
+from django.utils import timezone
+from django.utils.formats import date_format
 
 from rest_framework_gis.serializers import (
     GeometryField, GeoFeatureModelListSerializer
@@ -28,6 +33,7 @@ class CollectionSerializer(GeoJSONMixin, serializers.Serializer):
     id = serializers.SerializerMethodField(read_only=True)
     name = serializers.CharField()
     description = serializers.CharField()
+    details = serializers.SerializerMethodField(read_only=True)
     geometry = GeometryField(source='geo')
     kind = serializers.CharField()
     url = serializers.SerializerMethodField(read_only=True)
@@ -45,33 +51,82 @@ class CollectionSerializer(GeoJSONMixin, serializers.Serializer):
             )
         return ''
 
+    def get_details(self, obj):
+        if obj['kind'] == 'location':
+            return {
+                'address': obj['_address']
+            }
+        if obj['kind'] == 'event':
+            tz = timezone.get_current_timezone()
+            start = obj['_start'].astimezone(tz)
+            start = date_format(start, "DATETIME_FORMAT")
+            end = obj['_end'].astimezone(tz)
+            end = date_format(end, "TIME_FORMAT")
+            return {
+                'group': obj['_group'],
+                'start': obj['_start'],
+                'end': obj['_end'],
+                'start_format': start,
+                'end_format': end,
+            }
+        return {}
+
 
 class CollectionViewSet(viewsets.ViewSet):
     def list(self, request):
-        columns = ("id", "name", "description", "geo", "kind")
+        # WARNING: only change order with care
+        # https://code.djangoproject.com/ticket/28553
+        columns = (
+            "id", "name",
+            "description", "geo",
+            "_address",
+            "_group",
+            "_start",
+            "_end",
+            "kind",
+        )
 
-        # non_group_events = CollectionEvent.objects.filter(group=None)
         groups = (
             CollectionGroup.objects.all()
             .annotate(
+                _address=Value("", output_field=CharField()),
+                _group=Value(None, output_field=IntegerField()),
+                _start=Value(None, output_field=DateTimeField()),
+                _end=Value(None, output_field=DateTimeField()),
                 kind=Value("group", output_field=CharField()),
             )
-            .values(*columns)
+            .values_list(*columns)
         )
+
+        now = timezone.now()
+        two_weeks = now + timedelta(days=15)
         events = (
-            CollectionEvent.objects.all()
-            .annotate(
-                kind=Value("event", output_field=CharField())
+            CollectionEvent.objects.filter(
+                event_occurence__end__gte=now,
+                event_occurence__start__lte=two_weeks,
             )
-            .values(*columns)
+            .annotate(
+                _address=Value("", output_field=CharField()),
+                _group=F('group_id'),
+                _start=F('event_occurence__start'),
+                _end=F('event_occurence__end'),
+                kind=Value("event", output_field=CharField()),
+            )
+            .values_list(*columns)
         )
         locations = (
             CollectionLocation.objects.all()
             .annotate(
+                _address=F('address'),
+                _group=Value(None, output_field=IntegerField()),
+                _start=Value(None, output_field=DateTimeField()),
+                _end=Value(None, output_field=DateTimeField()),
                 kind=Value("location", output_field=CharField())
             )
-            .values(*columns)
+            .values_list(*columns)
         )
         qs = groups.union(events).union(locations)
+
+        qs = [dict(zip(columns, d)) for d in qs]
         serializer = CollectionSerializer(qs, many=True)
         return Response(serializer.data)
