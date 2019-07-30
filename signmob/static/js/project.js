09e18b9a12bf267f712064c77cd09716fdfde9dc
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-07-30 17:54:20 +0200

    Add resize iframe script to project

--- signmob/static/js/project.js
+++ signmob/static/js/project.js
@@ -1,1 +1,15 @@
 /* Project specific Javascript goes here. */
+(function(){
+
+  function resize () {
+    if (!window.parent || !window.parent.postMessage) {
+      return
+    }
+    var height = document.getElementsByTagName('html')[0].scrollHeight
+    window.parent.postMessage([document.location.search.substring(1), 'setHeight', height], '*')
+  }
+
+  document.addEventListener('resize', resize)
+  resize()
+
+}())
