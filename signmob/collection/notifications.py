58f4b20efff93990bb1bad1498144da3ca77f48e
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-08-02 14:54:47 +0200

    Trigger event created on CollectionEvent post_save

--- signmob/collection/notifications.py
+++ signmob/collection/notifications.py
@@ -1,14 +1,25 @@
 from django.dispatch import receiver
+from django.db.models import signals
 
 from .signals import (
     group_joined, location_created, location_reported,
     event_created,
 )
+from .models import CollectionEvent
 from .tasks import (
     location_created_task, location_reported_task,
     group_joined_task,
     event_created_task
 )
+
+
+@receiver(signals.post_save, sender=CollectionEvent)
+def trigger_event_created(sender, instance=None, created=False, **kwargs):
+    if kwargs.get('raw'):
+        return
+    if not created:
+        return
+    event_created.send(sender=CollectionEvent, user=None, event=instance)
 
 
 @receiver(location_created)
