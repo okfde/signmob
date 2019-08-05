e63f13fe58bbc73864f16ae9389db6103fc44e77
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2019-08-05 22:06:08 +0200

    Use transaction on commit hook to start tasks

--- signmob/collection/notifications.py
+++ signmob/collection/notifications.py
@@ -1,5 +1,6 @@
 from django.dispatch import receiver
 from django.db.models import signals
+from django.db import transaction
 
 from .signals import (
     group_joined, location_created, location_reported,
@@ -24,19 +25,19 @@ def trigger_event_created(sender, instance=None, created=False, **kwargs):
 
 @receiver(location_created)
 def notify_location_created(sender, location, **kwargs):
-    location_created_task.delay(location.id)
+    transaction.on_commit(lambda: location_created_task.delay(location.id))
 
 
 @receiver(location_reported)
 def notify_location_reported(sender, location, **kwargs):
-    location_reported_task.delay(location.id)
+    transaction.on_commit(lambda: location_reported_task.delay(location.id))
 
 
 @receiver(group_joined)
 def notify_group_joined(sender, user, group, **kwargs):
-    group_joined_task.delay(user.id, group.id)
+    transaction.on_commit(lambda: group_joined_task.delay(user.id, group.id))
 
 
 @receiver(event_created)
 def notify_event_created(sender, event, **kwargs):
-    event_created_task.delay(event.id)
+    transaction.on_commit(lambda: event_created_task.delay(event.id))
